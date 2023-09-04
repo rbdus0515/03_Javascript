f2958e939a60ca4a1eb87a8b2d14768dd6c75389
rbdus0515 <dnqkd707@gmail.com>
2023-09-04 15:06:25 +0900

    up1

---
+++ js/01_JS개요.js
@@ -0,0 +1,17 @@
+// 한줄주석
+/* 범위 주석 */
+
+// js 파일은 <script> 태그 내부라고 생각하면 됨
+
+function btnClick2() {
+    alert("extarnal 버튼이 클릭됨.")
+}
+
+function changeColor1() {
+    document.getElementById("box").style.backgroundColor = 'red';
+}
+
+function changeColor2() {
+    document.getElementById("box").style.backgroundColor = 'yellow';
+}
+
