5ef8e21b9a2ae4a153b87a090a4f3593ad8f27bb
rbdus0515 <dnqkd707@gmail.com>
2023-09-05 16:59:59 +0900

    up6

--- js/회원가입양식.js
+++ js/회원가입양식.js
@@ -1,82 +1,138 @@
-// form 내부에서 회원가입(submit) 버튼이 클릭된 경우
-
+// 유효성 검사 객체
+const checkObj = {
+    "inputId" : false, // 아이디
+    "inputPw" : false, // 비밀번호
+    "inputPwConfirm" : false, // 비번확인
+    "inputName" : false, // 이름
+    "gender" : false, // 성별
+    "inputTel" : false // 전화번호
+}
 
 // 아이디 확인
-document.getElementById("clickButton").addEventListener("click", function() {
-    
-    document.getElementById("inputId").addEventListener("click", function() {
+document.getElementById("inputId").addEventListener("change", function () {
 
-    const regExp = /^[a-z][\w\d][-][_]{6,14}/;
-
-    if( regExp.test(this.value) ) {
+    const regExp = /^[a-z][\w-_]{5,13}$/;
+                // 소문자시작(1) + 나머지(5~13) = 6~14글자
+    if (regExp.test(this.value)) {
         this.style.backgroundColor = "springgreen";
+        checkObj.inputId = true;
     } else {
         this.style.backgroundColor = "red";
         this.style.color = "white";
+        checkObj.inputId = false;
     }
-    });
-    
 });
+    
 
 // 비밀번호, 비밀번호 확인 : 키보드가 올라올 때
-// 성공
-document.getElementById("inputPwConfirm").addEventListener("keyup" , function() {
+const inputPw = document.getElementById("inputPw");
+const inputPwConfirm = document.getElementById("inputPwConfirm");
 
-    const span = document.getElementById("pwMessage");
+inputPwConfirm.addEventListener("keyup", function() {
 
+    if(inputPw.value.length == 0) {
+        this.value = "";
+        alert("비밀번호를 입력해주세요");
+        inputPw.focus();
+        checkObj.inputPw = false;
+    }
+});
 
-    if(inputPw.value == 0) {
-        alert("비밀번호를 입력해주세요.");
-        document.getElementById("inputPw").focus();
-        inputPwConfirm.value = null;
-        return;
+// 비밀번호 일치할 경우
+// 비밀번호 일치하지 않을 경우
+const pwMessage = document.getElementById("pwMessage");
+
+inputPw.addEventListener("keyup", function() {
+
+    if( (inputPw.value == inputPwConfirm.value) && inputPw.value.length != 0 ) {
+        pwMessage.innerText = "비밀번호 일치";
+        pwMessage.classList.add("confirm");
+        pwMessage.classList.remove("error");
+        checkObj.inputPw = true;
+        checkObj.inputPwConfirm = true;
+    } else {
+        pwMessage.innerText = "비밀번호 불일치";
+        pwMessage.classList.add("error");
+        pwMessage.classList.remove("confirm");
+        checkObj.inputPw = false;
+        checkObj.inputPwConfirm = false;
     }
 
-    if(inputPwConfirm.value == inputPw.value) {
-        span.innerText = "비밀번호 일치";
-        span.style.color = "green";
+});
+
+inputPwConfirm.addEventListener("keyup", function() {
+
+    if( (inputPw.value == inputPwConfirm.value) && inputPw.value.length != 0 ) {
+        pwMessage.innerText = "비밀번호 일치";
+        pwMessage.classList.add("confirm");
+        pwMessage.classList.remove("error");
+        checkObj.inputPw = true;
+        checkObj.inputPwConfirm = true;
     } else {
-        span.innerText = "비밀번호 불일치";
-        span.style.color = "red";
+        pwMessage.innerText = "비밀번호 불일치";
+        pwMessage.classList.add("error");
+        pwMessage.classList.remove("confirm");
+        checkObj.inputPw = false;
+        checkObj.inputPwConfirm = false;
     }
 
 });
 
 // 이름 확인
-// 성공?
-document.getElementById("inputName").addEventListener("keyup", function() {
+document.getElementById("inputName").addEventListener("change", function() {
 
-    const span = document.getElementById("nameMessage");
+    const regExp = /^[가-힣]{2,5}$/;
 
-    const regExp3 = /^[가-힣]{2,5}$/;
+    const nameMessage = document.getElementById("nameMessage");
     
-    if(this.value.length == 0) {
-        span.innerText = "";
-        return;
-    }
-
-    if (regExp3.test(this.value)) {
-        span.innerText = "정상입력";
-        span.style.color = "green";
+    if(regExp.test(this.value)) {
+        nameMessage.innerText = "정상입력";
+        nameMessage.classList.add("confirm");
+        nameMessage.classList.remove("error");
+        checkObj.inputName = true;
     } else {
-        span.innerText = "한글만 입력하세요.";
-        span.style.color = "red";
-    }
-});
-
-// 회원가입 버튼 클릭시 성별체크
-document.getElementById("uploade").addEventListener("click", function() {
-
-    
-});
-
-// 전화번호 형식이 올바르지 않을 경우
-document.getElementById("inputTel").addEventListener("click", function() {
-
-    const regExp4 = /^[0][0-9]{1,2}-[0-9]{3,4}-[0-9]{4}/;
-
-    if (regExp4.test(this.value)) {
-        alert('전화번호의 형식이 올바르지 않습니다.')
+        nameMessage.innerText = "한글만 입력하세요";
+        nameMessage.classList.add("error");
+        nameMessage.classList.remove("confirm");
+        checkObj.inputName = false;
     }
 
 });
+
+// 회원가입 버튼 클릭시
+function validate() {
+
+    // 성별이 선택되지 않은 경우
+    const gender = document.getElementsByName("gender");
+
+    if(!gender[0].checked && !gender[1].checkVisibility) {
+        alert("성별을 선택해주세요.");
+        checkObj.gender = false;
+
+        return false;
+    } else {
+        checkObj.gender = frue;
+    }
+    // 전화번호 형식이 올바르지 않을 경우
+    const inputTel = document.getElementById("inputTel");
+    const regExp = /^[0][0-9]{1,2}-[0-9]{3,4}-[0-9]{4}/;
+
+    if(!regExp.test(inputTel.value)) {
+        alert("전화번호의 형식이 올바르지 않습니다.");
+        checkObj.inputTel = false;
+        return false;
+    } else {
+        checkObj.inputTel = true;
+    }
+
+    // checkObj가 전부 true일때 == 모든 유효성 검사를 통과했을 때 ==> 회원가입
+    for(let key in checkObj) {
+        if( !ckeckObj[key] ) { // 1) checkObj["inputTel"] ==> false
+            return false;
+        }
+    }
+
+    alert("회원가입 완료")
+    return true;
+}
+
